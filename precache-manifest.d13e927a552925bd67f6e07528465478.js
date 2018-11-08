c7b46e4b21ba54e50e7df14ac25bc582abcb9f9e
AndriiSlobodianiuk <a.slobodianiuk@astoundcommerce.com>
2018-11-08 13:45:18 +0200

    Updates

---
+++ precache-manifest.d13e927a552925bd67f6e07528465478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c0be4d0c17943b2190b",
+    "url": "/React-app/static/js/runtime~main.3c0be4d0.js"
+  },
+  {
+    "revision": "cd68687518c2b1b512ba",
+    "url": "/React-app/static/js/main.cd686875.chunk.js"
+  },
+  {
+    "revision": "ed552849107bfdf90ec9",
+    "url": "/React-app/static/js/1.ed552849.chunk.js"
+  },
+  {
+    "revision": "cd68687518c2b1b512ba",
+    "url": "/React-app/static/css/main.b0bebc75.chunk.css"
+  },
+  {
+    "revision": "ed552849107bfdf90ec9",
+    "url": "/React-app/static/css/1.bf72bc68.chunk.css"
+  },
+  {
+    "revision": "43cde641d32f0f61c49da3aaa38b35c9",
+    "url": "/React-app/index.html"
+  }
+];
